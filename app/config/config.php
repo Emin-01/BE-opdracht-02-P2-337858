85d1045eff00a1c3e6e07a2b7783e37e6d28212f
Emin <337858@student.mboutrecht.nl>
2024-12-12 19:41:03 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * De database verbindingsgegevens
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'BE-Opdracht02');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+/**
+ * De naam van de virtualhost
+ */
+define('URLROOT', 'http://be-opdracht02');
+
+
+/**
+ * Het pad naar de folder app
+ */
+define('APPROOT', dirname(dirname(__FILE__)));
+
+
+
